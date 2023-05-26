6969a802952063970d9fd62962d7935c1a8cdf38
Damoon Imani <damoonimani@pm.me>
2023-05-26 16:05:24 +0330

    nvim changes

--- nvim/lua/user/set.lua
+++ nvim/lua/user/set.lua
@@ -9,9 +9,8 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
-
 vim.opt.smartindent = true
-
+vim.opt.mouse = nil
 vim.opt.wrap = false
 
 vim.g.mapleader = " "
